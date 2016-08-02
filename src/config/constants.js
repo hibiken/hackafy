2f1f90033cd7d2dfbb1b14d5e4e2ab32d5779dfa
Ken Hibino <ken.hibino7@gmail.com>
2016-08-01 21:45:40 -0700

    Fix production API_URL

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'production' ?
-                      'https://enigmatic-mountain-38641.herokuapp.com' :
+                      'https://enigmatic-mountain-38641.herokuapp.com/api' :
                       'http://localhost:5000/api';
