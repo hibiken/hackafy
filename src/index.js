70ece54d4dd99eed01f1ea247aad1be0814ae9b5
Ken Hibino <ken.hibino7@gmail.com>
2016-08-02 22:12:18 -0700

    Include CSSgram library

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/configureStore';
 import Root from './containers/Root';
+
+import '../styles/vendors/normalize.css';
+import '../styles/vendors/skeleton.css';
+import '../styles/vendors/cssgram.min.css';
 import './styles/index.css';
 
 const store = configureStore();
--- src/views/Home.js
+++ src/views/Home.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import PhotoGallery from '../containers/PhotoGallery';
 import NewPostButton from '../components/NewPostButton';
 
-import '../styles/vendors/normalize.css';
-import '../styles/vendors/skeleton.css';
 import '../styles/Home.css';
 
 class Home extends Component {
