3e917bb479c3a2b602cf7720661a048850d7f818
Ken Hibino <ken.hibino7@gmail.com>
2016-08-16 21:46:02 -0700

    Install react-router-scroll

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import {
+  Router,
+  Route,
+  IndexRoute,
+  browserHistory,
+  applyRouterMiddleware
+} from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import { useScroll } from 'react-router-scroll';
 
 import MainLayout from '../components/MainLayout';
 import Home from '../views/Home';
@@ -17,7 +24,7 @@ const createRoutes = (store) => {
   // Create an enhanced history that syncs navigation events with the store
   const history = syncHistoryWithStore(browserHistory, store);
   return (
-    <Router history={history}>
+    <Router history={history} render={applyRouterMiddleware(useScroll())}>
       <Route path="/" component={MainLayout}>
         <IndexRoute component={requireAuth(Home)} />
         <Route path="/signup" component={SignUp} />
