d95bb47c49f01ca2521d06a07efd77dcfd7bbd86
Ken Hibino <ken.hibino7@gmail.com>
2016-09-17 23:28:02 -0400

    Add propTypes to MainLayout component

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Header from '../containers/Header';
 import '../styles/MainLayout.css';
 
-class MainLayout extends React.Component {
-  render() {
-    return (
-      <div className="MainLayout__root">
-        <Header location={this.props.location} />
-        <div style={{minHeight: '100vh'}}>
-          {this.props.children}
-        </div>
+const MainLayout = (props) => {
+  return (
+    <div className="MainLayout__root">
+      <Header location={props.location} />
+      <div style={{minHeight: '100vh'}}>
+        {props.children}
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+MainLayout.propTypes = {
+  location: PropTypes.object.isRequired,
+  children: PropTypes.element.isRequired,
 }
 
 export default MainLayout;
