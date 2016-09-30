73450b4d93f8c50440defdeec56a962355089721
Ken Hibino <ken.hibino7@gmail.com>
2016-09-30 11:03:14 -0400

    Install Material UI

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,12 +1,15 @@
 import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import createRoutes from '../routes';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const Root = ({store}) => {
   const routes = createRoutes(store);
   return (
     <Provider store={store}>
-      {routes}
+      <MuiThemeProvider>
+        {routes}
+      </MuiThemeProvider>
     </Provider>
   );
 };
