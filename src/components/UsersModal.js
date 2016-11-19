f8bf5d1e0ede536c2dc8924af108ff0177259ef5
Ken Hibino <ken.hibino7@gmail.com>
2016-11-19 18:36:50 -0500

    Update follow button styles

--- src/components/UsersModal.js
+++ src/components/UsersModal.js
@@ -46,7 +46,7 @@ class UsersModal extends Component {
         left: 0,
         right: 0,
         bottom: 0,
-        backgroundColor: 'rgba(0, 0, 0, 0.75)'
+        backgroundColor: 'rgba(0, 0, 0, 0.5)'
       },
       content : {
         position: 'absolute',
